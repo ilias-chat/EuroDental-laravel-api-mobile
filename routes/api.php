42c99f7413b581322d0d02f31035e002227ab3de
ilias-chat <ilias.chat.7@gmail.com>
2026-08-13 22:44:43 +0200

    Expose mobile VAPID public key without auth

--- routes/api.php
+++ routes/api.php
@@ -26,6 +26,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/webpush/vapid-public-key', [WebPushController::class, 'publicKey']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
@@ -40,7 +41,6 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('/push-token', [PushTokenController::class, 'store']);
     Route::delete('/push-token', [PushTokenController::class, 'destroy']);
-    Route::get('/webpush/vapid-public-key', [WebPushController::class, 'publicKey']);
     Route::post('/webpush/subscribe', [WebPushController::class, 'subscribe']);
     Route::post('/webpush/unsubscribe', [WebPushController::class, 'unsubscribe']);
 
